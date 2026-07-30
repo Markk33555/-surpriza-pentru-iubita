507c37a313543a01ed878e05e597d095bcf546a9
Markk33555 <164097808+Markk33555@users.noreply.github.com>
2026-07-31 00:19:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,29 @@
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-}
+const button = document.getElementById("startButton");
 
-body{
-    height:100vh;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    background:linear-gradient(180deg,#020111,#071330,#111c44);
-    overflow:hidden;
-    font-family:Arial,Helvetica,sans-serif;
-    color:white;
-}
+button.addEventListener("click", () => {
 
-.hero{
-    width:90%;
-    max-width:700px;
-    text-align:center;
-    padding:40px;
-    background:rgba(255,255,255,0.08);
-    border-radius:25px;
-    backdrop-filter:blur(12px);
-    box-shadow:0 0 40px rgba(255,255,255,.12);
-}
+    document.querySelector(".hero").innerHTML = `
 
-h1{
-    font-size:2.3rem;
-    margin-bottom:20px;
-}
+    <h1>❤️ Mulțumesc ❤️</h1>
 
-p{
-    font-size:1.2rem;
-    line-height:1.8;
-    color:#ececec;
-    margin-bottom:35px;
-}
+    <p style="margin-top:30px;font-size:22px;line-height:1.8">
 
-button{
-    padding:16px 34px;
-    border:none;
-    border-radius:50px;
-    background:#ff4f81;
-    color:white;
-    font-size:1.1rem;
-    cursor:pointer;
-    transition:.3s;
-}
+    Nu am făcut acest site pentru că este o zi specială.
 
-button:hover{
-    transform:scale(1.08);
-    box-shadow:0 0 25px #ff4f81;
-}
+    L-am făcut pentru că tu ești o persoană specială.
+
+    Îți mulțumesc pentru fiecare zâmbet.
+
+    Îți mulțumesc pentru fiecare îmbrățișare.
+
+    Îți mulțumesc că îmi ești alături.
+
+    Sunt recunoscător în fiecare zi că faci parte din viața mea.
+
+    Te iubesc din tot sufletul. ❤️
+
+    </p>
+
+    `;
+
+});
